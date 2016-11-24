c4d9f18d66ae418ea89088c830b680a49ffca7e8
dawoodchouhan <dawoodchauhan.93@gmail.com>
2016-11-24 23:00:49 +0530

    Second Commit

---
+++ src/main/java/com/niit/collaborationbackend/dao/FriendsDAO.java
@@ -0,0 +1,25 @@
+package com.niit.collaborationbackend.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.niit.collaborationbackend.model.Friends;
+
+
+
+@Repository
+public interface FriendsDAO {
+
+	public List<Friends> getMyFriend(String id);
+	
+	public Friends get(String userID, String friendID);
+	
+		public boolean save(Friends friends);
+		public boolean update(Friends friends);
+		public boolean delete(String userID, String friendID);
+		public List<Friends> getNewFriendRequests(String userID);
+		public void setOnline(String userID);
+		public void setOffLine(String loggedInUserID);
+	
+}
