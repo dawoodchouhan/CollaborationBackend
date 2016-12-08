0d5bca0b4118659be10618e64aac3d9780ec92bf
dawoodchouhan <dawoodchauhan.93@gmail.com>
2016-12-08 23:27:06 +0530

    BlogComment

---
+++ src/main/java/com/niit/collaborationbackend/dao/BlogCommentDAO.java
@@ -0,0 +1,19 @@
+package com.niit.collaborationbackend.dao;
+
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.niit.collaborationbackend.model.BlogComment;
+
+
+
+@Repository("blogCommentDAO")
+public interface BlogCommentDAO {
+
+	
+	public boolean save(BlogComment blogComment);
+	public List<BlogComment> list();
+	public BlogComment get(int id); 
+	
+}
